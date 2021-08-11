ed9d7921d543d99179b97a4af5d0e3e9105dc4b4
CristianGama <88747040+CristianGama@users.noreply.github.com>
2021-08-11 15:19:30 -0500

    Update localscript.lua

--- localscript.lua
+++ localscript.lua
@@ -2,10 +2,26 @@ local letra = game:GetService("UserInputService")
 local evento = script.Parent:WaitForChild("Activacion")
 local aux = true
 local arma = script.Parent
+local activado = true
+local MouseEvent = arma:WaitForChild("Raton")
+
+arma.Equipped:Connect(function(mouse)
+	
+	activado = true
+	
+	while activado do
+		wait()
+		MouseEvent:FireServer(mouse.Hit.LookVector)
+	end
+	
+end)
 
 letra.InputBegan:Connect(function(tecla)
 
 	arma.Unequipped:Connect(function()
+		
+		activado = false
+		
 		if tecla.KeyCode == Enum.KeyCode.R and aux == true then
 
 			aux = false
@@ -14,7 +30,9 @@ letra.InputBegan:Connect(function(tecla)
 			aux = true
 
 		end
+		
 	end)
+	
 	if tecla.KeyCode == Enum.KeyCode.R and aux == true then
 
 		aux = false
